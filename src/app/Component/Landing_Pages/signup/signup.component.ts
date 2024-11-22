3fdb88c7da908c46209ee5d2f594e3f9d30902c6
Anojan1520 <unicomtic1520@gmail.com>
2024-11-22 11:18:27 +0530

    import Missing Modules In SignUp Ts

--- src/app/Component/Landing_Pages/signup/signup.component.ts
+++ src/app/Component/Landing_Pages/signup/signup.component.ts
@@ -1,12 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule,ReactiveFormsModule,CommonModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })

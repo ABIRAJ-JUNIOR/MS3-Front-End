22c81612d454fd5b0f803f547dd30a58df86cbf6
ABIRAJ-JUNIOR <abirajjr2002@gmail.com>
2024-11-23 15:44:11 +0530

    "Added jwtDecode import and implemented role-based boolean flags in Navebar01Component"

--- src/app/Component/common_components/navebar-01/navebar-01.component.ts
+++ src/app/Component/common_components/navebar-01/navebar-01.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { TopinfoComponent } from '../topinfo/topinfo.component';
+import { jwtDecode } from 'jwt-decode';
 
 @Component({
   selector: 'app-navebar-01',
@@ -10,9 +11,31 @@ import { TopinfoComponent } from '../topinfo/topinfo.component';
   styleUrl: './navebar-01.component.css'
 })
 export class Navebar01Component {
+  isAdmin!:boolean;
+  isStudent!:boolean;
 
   sidebarCollapsed = false;
+
+  constructor(){
+   const token:string = localStorage.getItem("token")!;
+   const decode:any = jwtDecode(token)
+
+   if(decode.Role == "Administrator" || decode.Role == "Instructor"){
+    this.isAdmin = true
+    this.isStudent = false
+   }if(decode.Role =="Student"){
+    this.isAdmin = false
+    this.isStudent = true
+   }
+
+  }
+
+
   toggleSidebar() {
     this.sidebarCollapsed = !this.sidebarCollapsed;
   }
+
+
+
+
 }

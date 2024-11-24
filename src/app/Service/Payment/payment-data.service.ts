52eb34fdcaa1477ad91c482a92ca7c7e984872d2
Anojan1520 <unicomtic1520@gmail.com>
2024-11-24 21:50:24 +0530

    Add Send paymentData method On Course Card Component

--- src/app/Service/Payment/payment-data.service.ts
+++ src/app/Service/Payment/payment-data.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PaymentDataService {
 
-  constructor() { }
+  // Initializing an empty array as the default value
+  private dataSource = new BehaviorSubject<any[]>([]); // Array of any type (can be replaced with specific type)
+  currentData = this.dataSource.asObservable(); // Expose the array as an observable
+
+  constructor() {}
+
+  // Method to change the array data
+  changeData(data: any[]) {
+    this.dataSource.next(data); // Update the array with new data
+  }
 }

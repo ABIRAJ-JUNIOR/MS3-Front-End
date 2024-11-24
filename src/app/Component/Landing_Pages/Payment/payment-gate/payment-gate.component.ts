05bee1f1705b13b37ee9963ed613d90ad213926d
Anojan1520 <unicomtic1520@gmail.com>
2024-11-25 01:42:27 +0530

    Implement Card Modal and update ui

--- src/app/Component/Landing_Pages/Payment/payment-gate/payment-gate.component.ts
+++ src/app/Component/Landing_Pages/Payment/payment-gate/payment-gate.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { PaymentDataService } from '../../../../Service/Payment/payment-data.service';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-payment-gate',
   standalone: true,
-  imports: [CommonModule,FormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './payment-gate.component.html',
   styleUrl: './payment-gate.component.css'
 })
@@ -14,18 +14,49 @@ export class PaymentGateComponent {
 
   recievedModalItems: any[] = []; // Array to hold the received data
 
-  constructor(private PaymentDataService: PaymentDataService) {
-    
-   }
+  CardFormData: FormGroup;
 
-  ngOnInit():void{
+  constructor(private PaymentDataService: PaymentDataService, private fb: FormBuilder) {
+
+    this.CardFormData = this.fb.group({
+      name: ['', [Validators.required]],
+      number: ['',[
+          Validators.required,
+          Validators.pattern('^[0-9]{13,19}$'), 
+        ],
+      ],
+      expiration: ['', [Validators.required]],
+      cvv: ['',[
+          Validators.required,
+          Validators.maxLength(3),
+          Validators.minLength(3),
+          Validators.pattern('^[0-9]{3}$'), 
+        ],
+      ],
+    });
+
+
+
+
+  }
+
+  ngOnInit(): void {
 
     this.loadItems()
 
   }
- 
-  loadItems():void{
-    this.recievedModalItems.push(JSON.parse( this.PaymentDataService.PurchaseDetailGetLocal() ))
+
+  loadItems(): void {
+    this.recievedModalItems.push(JSON.parse(this.PaymentDataService.PurchaseDetailGetLocal()))
     console.log(this.recievedModalItems)
   }
+
+
+
+  CardDetails: any[] = []
+
+  AddCardDetails() {
+
+  }
+
 }

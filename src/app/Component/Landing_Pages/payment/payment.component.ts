aab78d5862f002df3cdb52ad6bc3c10d50b41456
Anojan1520 <unicomtic1520@gmail.com>
2024-11-24 21:52:11 +0530

    Add Recive Data Handle in Payment Component Method

--- src/app/Component/Landing_Pages/payment/payment.component.ts
+++ src/app/Component/Landing_Pages/payment/payment.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PaymentDataService } from '../../../Service/Payment/payment-data.service';
 
 @Component({
   selector: 'app-payment',
@@ -8,5 +9,13 @@ import { Component } from '@angular/core';
   styleUrl: './payment.component.css'
 })
 export class PaymentComponent {
+  recievedModalItems: string[] = []; // Array to hold the received data
 
+  constructor(private PaymentDataService: PaymentDataService) { }
+
+  ngOnInit() {
+    this.PaymentDataService.currentData.subscribe((data) => {
+      this.recievedModalItems = data; // Update the array with received data
+    });
+  }
 }

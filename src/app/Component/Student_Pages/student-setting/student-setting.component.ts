1fe3c3d7e193a8f748545642a5de44ed40f2fd61
Anojan1520 <unicomtic1520@gmail.com>
2024-11-26 19:56:30 +0530

    Implement Editmode For ReactiveFormsModule

--- src/app/Component/Student_Pages/student-setting/student-setting.component.ts
+++ src/app/Component/Student_Pages/student-setting/student-setting.component.ts
@@ -3,12 +3,12 @@ import { Component, OnInit } from '@angular/core';
 import { StudentDashDataServiceService } from '../../../Service/Student/student-dash-data-service.service';
 import { Student } from '../../../Modals/modals';
 import { StudentService } from '../../../Service/Student/student.service';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-student-setting',
   standalone: true,
-  imports: [CommonModule ,ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './student-setting.component.html',
   styleUrl: './student-setting.component.css'
 })
@@ -17,15 +17,24 @@ export class StudentSettingComponent implements OnInit {
 
   IsEditMode: boolean = false;
   StudentTokenDetails: any;
-  studentForm:FormGroup;
+  studentForm: FormGroup;
 
-  constructor(private StudentDashDataService: StudentDashDataServiceService, private StudentApiService: StudentService ,private fb : FormBuilder) {
+  constructor(private StudentDashDataService: StudentDashDataServiceService, private StudentApiService: StudentService, private fb: FormBuilder) {
 
+    this.studentForm = this.fb.group({
+      firstName: ['Anojan'],
+      lastName: ['anoj'],
+      phone: ['+1 234 567 890'],
+      address: ['1234 Main St, City, Country'],
+      dateOfBirth: ['2004/09/30'],
+      gender: ['male'] // Initialize gender field
+    });
   }
 
 
   StudentDetails: any;
   ngOnInit(): void {
+    this.studentForm.disable();
     this.StudentTokenDetails = this.StudentDashDataService.GetStudentDeatilByLocalStorage();
 
     this.StudentApiService.getStudent(this.StudentTokenDetails.Id).subscribe((student: Student) => {
@@ -41,6 +50,12 @@ export class StudentSettingComponent implements OnInit {
 
   changeEditMode() {
     this.IsEditMode = !this.IsEditMode
+
+    if (this.IsEditMode) {
+      this.studentForm.enable();
+    } else {
+      this.studentForm.disable();
+    }
   }
 
 

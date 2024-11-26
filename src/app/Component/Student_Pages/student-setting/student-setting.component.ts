74ab7cfbf9bf797a066fe599e63f3776a891142c
Anojan1520 <unicomtic1520@gmail.com>
2024-11-26 19:34:41 +0530

    Implement NgOn Init and call api and assign Value In StudentDetails

--- src/app/Component/Student_Pages/student-setting/student-setting.component.ts
+++ src/app/Component/Student_Pages/student-setting/student-setting.component.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { StudentDashDataServiceService } from '../../../Service/Student/student-dash-data-service.service';
+import { Student } from '../../../Modals/modals';
+import { StudentService } from '../../../Service/Student/student.service';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-student-setting',
   standalone: true,
-  imports: [],
+  imports: [CommonModule ,ReactiveFormsModule],
   templateUrl: './student-setting.component.html',
   styleUrl: './student-setting.component.css'
 })
-export class StudentSettingComponent {
+export class StudentSettingComponent implements OnInit {
+
+
+  IsEditMode: boolean = false;
+  StudentTokenDetails: any;
+  studentForm:FormGroup;
+
+  constructor(private StudentDashDataService: StudentDashDataServiceService, private StudentApiService: StudentService ,private fb : FormBuilder) {
+
+  }
+
+
+  StudentDetails: any;
+  ngOnInit(): void {
+    this.StudentTokenDetails = this.StudentDashDataService.GetStudentDeatilByLocalStorage();
+
+    this.StudentApiService.getStudent(this.StudentTokenDetails.Id).subscribe((student: Student) => {
+      this.StudentDetails = student
+
+    }
+      ,
+      (error) => {
+      })
+
+  }
+
+
+  changeEditMode() {
+    this.IsEditMode = !this.IsEditMode
+  }
+
 
 }

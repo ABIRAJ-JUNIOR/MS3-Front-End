4aacbb436a0aa943012b921d04bff82b168b31fc
AkeelAriskhan <akeelariskhan1314@gmail.com>
2024-11-28 18:38:06 +0530

    "Added imports and initialized announcement form with validation rules in AnnouncementComponent"

--- src/app/Component/Admin_Pages/announcement/announcement.component.ts
+++ src/app/Component/Admin_Pages/announcement/announcement.component.ts
@@ -1,12 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-announcement',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,ReactiveFormsModule],
   templateUrl: './announcement.component.html',
   styleUrl: './announcement.component.css'
 })
 export class AnnouncementComponent {
-
+  announcementForm!: FormGroup;
+  constructor(private fb: FormBuilder) {}
+  ngOnInit(): void {
+    // Initialize the form with validation rules
+    this.announcementForm = this.fb.group({
+      title: ['', [Validators.required, Validators.minLength(3)]],
+      expirationDate: ['', Validators.required],
+      audienceType: ['', Validators.required]
+    });
+  }
 }

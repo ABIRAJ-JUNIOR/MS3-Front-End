6f36a55ebd3737eebe0df72bb4fc0d9b57f7fa9c
ABIRAJ-JUNIOR <abirajjr2002@gmail.com>
2024-12-01 18:57:32 +0530

    "Added filter by evaluation dropdown to student assessments component and updated search pipe to filter by evaluated/not evaluated status"

--- src/app/Pipes/search-student-assessment.pipe.ts
+++ src/app/Pipes/search-student-assessment.pipe.ts
@@ -10,6 +10,14 @@ export class SearchStudentAssessmentPipe implements PipeTransform {
   transform(studentAssessments: StudentAssessment[], ...args: string[]): StudentAssessment[] {
     let searchTest:string = args[0]
 
+    if(searchTest == 'Evaluated'){
+      return studentAssessments.filter(sa => sa.marksObtaines != null);
+    }
+
+    if(searchTest == 'NotEvaluate'){
+      return studentAssessments.filter(sa => sa.marksObtaines == null);
+    }
+
     if(searchTest != ''){
       return studentAssessments.filter(sa => sa.assessmentResponse.courseResponse.courseName == searchTest)
     }else{

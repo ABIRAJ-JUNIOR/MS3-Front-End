d48f17f7a2aca7ea8982667aacb12210c139c146
Anojan1520 <unicomtic1520@gmail.com>
2024-12-02 01:01:17 +0530

    "Updated student dashboard component: modified CSS, HTML, and TS files to change sidebar width, add student profile information, and update navigation links and layout."

--- src/app/Component/Student_Pages/student-dashboard/student-dashboard.component.ts
+++ src/app/Component/Student_Pages/student-dashboard/student-dashboard.component.ts
@@ -7,17 +7,17 @@ import { Student } from "../../../Modals/modals";
 import { StudentService } from "../../../Service/API/Student/student.service";
 import { NotificationServiceService } from "../../../Service/API/Notification/notification-service.service";
 import { ToastrService } from "ngx-toastr";
-import { CourseService } from "../../../Service/API/Course/course.service";
 
 @Component({
   selector: 'app-student-dashboard',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, FormsModule, RouterModule],
+  imports: [RouterOutlet, CommonModule, FormsModule, RouterModule,],
   templateUrl: './student-dashboard.component.html',
   styleUrl: './student-dashboard.component.css'
 })
 export class StudentDashboardComponent implements OnInit {
   sidebarCollapsed = false;
+  noImage: string='https://www.bing.com/ck/a?!&&p=7648fe0c3dc6be6b2188d54e324d5d9352d68f37b129c4ddaaaadf8174ec11a4JmltdHM9MTczMzAxMTIwMA&ptn=3&ver=2&hsh=4&fclid=1a548757-8fff-66c7-3ffe-93498efe67cc&u=a1L2ltYWdlcy9zZWFyY2g_cT1wcm9maWxlJTIwbm8lMjBpbWFnZSZGT1JNPUlRRlJCQSZpZD05MzAzNEM1QTRDQjMyOTE2NzIxNzM3Q0Y2NzE0NDI3NDU1MDRDRTMx&ntb=1';
   toggleSidebar() {
     this.sidebarCollapsed = !this.sidebarCollapsed;
   }

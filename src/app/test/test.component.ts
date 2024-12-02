a34a9b996678a61cfaf331e50173451b1ba96b9e
ABIRAJ-JUNIOR <abirajjr2002@gmail.com>
2024-12-02 21:01:05 +0530

    "Updated admin dashboard component with announcement dropdown, modified signin component, and removed test component."

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,10 +1,7 @@
 import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
-import { FormGroup, FormBuilder, Validators, ReactiveFormsModule } from "@angular/forms";
-import { Router, RouterModule } from "@angular/router";
-import { ToastrService } from "ngx-toastr";
-import { AuthService } from "../Service/API/Auth/auth.service";
-
+import { ReactiveFormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
 
 
 @Component({
@@ -15,38 +12,7 @@ import { AuthService } from "../Service/API/Auth/auth.service";
   styleUrl: './test.component.css'
 })
 export class TestComponent {
-  textToType: string = "Your Sign-In Can Change the World";
-  displayedText: string = ""; 
-  typingSpeed: number = 100; 
-
-  signinForm: FormGroup
-
-  constructor(private fb: FormBuilder , private auth:AuthService , private rout:Router , private toastr:ToastrService) {
-    this.signinForm = this.fb.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required]]
-    })
-  }
-  ngOnInit(): void {
-    this.startTypingEffect();
-  }
-
-  startTypingEffect(): void {
-    let index = 0;
-
-    const typingInterval = setInterval(() => {
-      if (index < this.textToType.length) {
-        this.displayedText += this.textToType[index];
-        index++;
-      } else {
-        clearInterval(typingInterval);
-      }
-    }, this.typingSpeed);
-  }
-
-  onSubmit(){
-    
-  }
+ 
 }
   
 

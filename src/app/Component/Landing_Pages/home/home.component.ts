965a3f79a1cdde5a0b6f04ad08a9ab5c10db769a
ABIRAJ-JUNIOR <abirajjr2002@gmail.com>
2024-12-02 18:19:16 +0530

    "Updated admin dashboard and home components, changed course properties, and added course service method"

--- src/app/Component/Landing_Pages/home/home.component.ts
+++ src/app/Component/Landing_Pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FooterComponent } from '../../common_components/footer/footer.component';
 import { Navebar01Component } from '../../common_components/navebar-01/navebar-01.component';
 import { AuthService } from '../../../Service/API/Auth/auth.service';
 import { Router, RouterModule } from '@angular/router';
+import { CourseService } from '../../../Service/API/Course/course.service';
 
 
 @Component({
@@ -13,13 +14,17 @@ import { Router, RouterModule } from '@angular/router';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   isAdmin:boolean = false;
   isStudent:boolean = false;
 
   sidebarCollapsed = false;
+  topCourses:any[] = [];
 
-  constructor(private authService:AuthService){
+  constructor(
+    private authService:AuthService,
+    private courseService:CourseService
+  ){
     if(authService.isLoggedInAdmin()){
       this.isAdmin = true
       this.isStudent = false
@@ -31,32 +36,11 @@ export class HomeComponent {
     }
   }
 
-  topCourses = [
-    {
-      title: 'Full-Stack Web Development',
-      description: 'Learn to build modern websites and web apps using the latest technologies.',
-      image: 'https://res.cloudinary.com/dgpyq5til/image/upload/v1732516829/card1_otzkkd.jpg',
-      rating: 4.9,
-      feedbackCount: 1200,
-      link: '/courses/fullstack'
-    },
-    {
-      title: 'Data Science & AI',
-      description: 'Dive into data analysis, machine learning, and artificial intelligence.',
-      image: 'https://res.cloudinary.com/dgpyq5til/image/upload/v1732516829/card1_otzkkd.jpg',
-      rating: 4.8,
-      feedbackCount: 900,
-      link: '/courses/datascience'
-    },
-    {
-      title: 'Cybersecurity Essentials',
-      description: 'Master the art of protecting digital systems from cyber threats.',
-      image: 'https://res.cloudinary.com/dgpyq5til/image/upload/v1732516829/card1_otzkkd.jpg',
-      rating: 4.7,
-      feedbackCount: 800,
-      link: '/courses/cybersecurity'
-    }
-
-  ];
-
+  ngOnInit(): void {
+    this.courseService.getTop3Courses().subscribe({
+      next:(response:any)=>{
+        this.topCourses = response
+      }
+    })
+  }
 }

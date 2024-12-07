a2b12da23695b71cae3093524889e216b13a8b45
Anojan1520 <unicomtic1520@gmail.com>
2024-12-07 23:53:30 +0530

    `Added new layout and functionality to course-sechdules component, including grid layout, product details, and add to cart form. Updated course-sechdules component to fetch course details by course ID from course service.`

--- src/app/Component/Landing_Pages/course-sechdules/course-sechdules.component.ts
+++ src/app/Component/Landing_Pages/course-sechdules/course-sechdules.component.ts
@@ -1,14 +1,39 @@
 import { Component } from '@angular/core';
 import { Navebar01Component } from '../../common_components/navebar-01/navebar-01.component';
 import { FooterComponent } from '../../common_components/footer/footer.component';
+import { ActivatedRoute } from '@angular/router';
+import { CourseService } from '../../../Service/API/Course/course.service';
+import { Course } from '../../../Modals/modals';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-course-sechdules',
   standalone: true,
-  imports: [Navebar01Component,FooterComponent],
+  imports: [Navebar01Component, FooterComponent , CommonModule],
   templateUrl: './course-sechdules.component.html',
   styleUrl: './course-sechdules.component.css'
 })
 export class CourseSechdulesComponent {
 
+  constructor(private route: ActivatedRoute, private courseService: CourseService) { }
+
+  courseId: any;
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((params) => {
+      this.courseId = params.get('courseId');
+      this.courseGetById()
+    });
+  }
+
+  courses:any;
+
+  courseGetById() {
+    this.courseService.getCourseByID(this.courseId).subscribe({
+      next: (response: Course) => {
+        this.courses = response
+        console.log(this.courses)
+      }
+    })
+  }
 }

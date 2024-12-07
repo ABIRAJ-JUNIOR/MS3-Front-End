b0681f7a48755816862a44ca515cf55738caf352
ABIRAJ-JUNIOR <abirajjr2002@gmail.com>
2024-12-07 23:08:38 +0530

    `Added functionality to download invoice as PNG image`

--- src/app/Component/common_components/invoice/invoice.component.ts
+++ src/app/Component/common_components/invoice/invoice.component.ts
@@ -5,6 +5,7 @@ import { Transaction } from '../../Admin_Pages/payments-overview/payments-overvi
 import { StudentService } from '../../../Service/API/Student/student.service';
 import { ResolveEnd } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-invoice',
@@ -45,6 +46,25 @@ export class InvoiceComponent implements OnInit{
   }
 
   downloadInvoice() {
+    const element = document.getElementById('invoiceContent');
+    if (!element) {
+      console.error('Element not found!');
+      return;
+    }
+    html2canvas(element, {
+      scale: 3,
+      useCORS: true,
+      backgroundColor: '#ffffff',
+    })
+      .then((canvas:any) => {
+        const imageData = canvas.toDataURL('image/png', 1.0);
+
+        const link = document.createElement('a');
+        link.href = imageData;
+        link.download = `${this.studentData.firstName}-${this.studentData.lastName}.png`;
+        link.click();
+      })
+      .catch((err:any) => console.error('Error generating high-quality PNG:', err));
   }
 
   sendEmail() {

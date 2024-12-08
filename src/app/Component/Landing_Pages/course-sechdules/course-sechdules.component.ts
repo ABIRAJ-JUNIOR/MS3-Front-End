6d7db86cffffe3c3f2fc8eb4cd5bebf69b124414
Anojan1520 <unicomtic1520@gmail.com>
2024-12-09 00:58:54 +0530

    `Added recent feedbacks section and student service to course-sechdules component`

--- src/app/Component/Landing_Pages/course-sechdules/course-sechdules.component.ts
+++ src/app/Component/Landing_Pages/course-sechdules/course-sechdules.component.ts
@@ -7,6 +7,7 @@ import { Course } from '../../../Modals/modals';
 import { CommonModule } from '@angular/common';
 import { PaymentDataService } from '../../../Service/Data/Payment_Data/payment-data.service';
 import { TopinfoComponent } from "../../common_components/topinfo/topinfo.component";
+import { StudentService } from '../../../Service/API/Student/student.service';
 
 @Component({
   selector: 'app-course-sechdules',
@@ -20,7 +21,8 @@ export class CourseSechdulesComponent {
   constructor(private route: ActivatedRoute,
     private courseService: CourseService,
     private paymentService: PaymentDataService,
-    private router: Router
+    private router: Router,
+    private studentService: StudentService,
   ) { }
 
   courseId: any;
@@ -31,6 +33,7 @@ export class CourseSechdulesComponent {
       this.courseGetById()
     });
     this.getAllCourses();
+    this.allstudentLoad();
   }
 
   courses: any;
@@ -79,6 +82,16 @@ export class CourseSechdulesComponent {
     } else {
       this.CourseRating = 0;
     }
+  }
+  AllStudents: any;
+  allstudentLoad() {
+    this.studentService.getStudents().subscribe({
+      next: (response: any) => {
+        this.AllStudents = response
+      }, error: (error) => {
+        console.log(error.message)
+      }
+    })
   }
 
   PaymentCourse: any[] = []

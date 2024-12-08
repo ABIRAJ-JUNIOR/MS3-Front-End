e0e112fec5e62787e3f80f783bbdf7d7eab55b22
Anojan1520 <unicomtic1520@gmail.com>
2024-12-09 00:27:20 +0530

    `Updated biomatrics component HTML and TS files with minor styling and routing changes`

--- src/app/Component/common_components/biomatrics/biomatrics.component.ts
+++ src/app/Component/common_components/biomatrics/biomatrics.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { WindowAuthService } from '../../../Service/Biomatrics/window-auth.service';
 import { CommonModule } from '@angular/common';
 import { WindowDataService } from '../../../Service/Biomatrics/window-data.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-biomatrics',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,RouterModule],
   templateUrl: './biomatrics.component.html',
   styleUrl: './biomatrics.component.css'
 })

12545dbd6c2073a6bc3a326d14e12c8c85f18947
Anojan1520 <unicomtic1520@gmail.com>
2024-12-11 12:02:34 +0530

    `Added HttpClient and API endpoints to otp-verfication.service.ts`

--- src/app/Service/API/OTP/otp-verfication.service.ts
+++ src/app/Service/API/OTP/otp-verfication.service.ts
@@ -1,9 +1,28 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../../../environments/environment.development';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OtpVerficationService {
 
-  constructor() { }
+  apiUrl:string=environment.apiUrl
+  constructor(private http:HttpClient) { }
+
+   changePassword(payload:any): Observable<any> {
+    const endpoint = `${this.apiUrl}/changePassword`;
+    return this.http.post(endpoint, payload);
+  }
+
+  sendOtp(payload:any): Observable<any> {
+    const endpoint = `${this.apiUrl}/sendOtp`;
+    return this.http.post(endpoint, payload);
+  }
+
+  verifyOtp(payload:any): Observable<any> {
+    const endpoint = `${this.apiUrl}/verifyOtp`;
+    return this.http.post(endpoint, payload);
+  }
 }

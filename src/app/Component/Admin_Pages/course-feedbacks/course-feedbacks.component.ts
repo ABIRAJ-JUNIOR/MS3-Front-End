31ae26b0295c7a3fcf9e707af974bc4b5d2f146d
ABIRAJ-JUNIOR <abirajjr2002@gmail.com>
2024-12-12 18:08:22 +0530

    `Refactor pagination layout and functionality in course-feedbacks component`

--- src/app/Component/Admin_Pages/course-feedbacks/course-feedbacks.component.ts
+++ src/app/Component/Admin_Pages/course-feedbacks/course-feedbacks.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { FeedbackServiceService } from '../../../Service/API/Feedback/feedback-service.service';
 import { FeedBack } from '../../../Modals/modals';
@@ -11,10 +11,10 @@ import { FeedBack } from '../../../Modals/modals';
   templateUrl: './course-feedbacks.component.html',
   styleUrl: './course-feedbacks.component.css'
 })
-export class CourseFeedbacksComponent {
+export class CourseFeedbacksComponent implements OnInit {
   feedbacks:FeedBack[] = [];
   currentPage: number = 1;
-  pageSize: number = 8;
+  pageSize: number = 9;
   totalPages: number = 0;
   currentLength: number = 0;
   totalItems: number = 0;
@@ -29,7 +29,12 @@ export class CourseFeedbacksComponent {
   private loadFeedBacks():void{
     this.feedbackService.pagination(this.currentPage,this.pageSize).subscribe({
       next:(response:any)=>{
-        this.feedbacks = response;
+        this.feedbacks = response.items;
+        this.totalPages = response.totalPages;
+        this.totalItems = response.totalItem;
+      },
+      complete:()=>{
+        this.currentLength = this.feedbacks.length;
       },
       error:(error:any)=>{
         console.log(error.error);

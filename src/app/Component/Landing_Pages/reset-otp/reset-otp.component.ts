086f3f40b4770e350d0db2641595f57dbde2a690
ABIRAJ-JUNIOR <abirajjr2002@gmail.com>
2024-12-13 17:16:39 +0530

    `feat: updated reset-otp component html and ts files with new password and confirm password fields`

--- src/app/Component/Landing_Pages/reset-otp/reset-otp.component.ts
+++ src/app/Component/Landing_Pages/reset-otp/reset-otp.component.ts
@@ -1,6 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { OtpVerficationService } from '../../../Service/API/OTP/otp-verfication.service';
@@ -9,9 +15,9 @@ import { passwordValidator } from '../../Admin_Pages/account-setting/account-set
 @Component({
   selector: 'app-reset-otp',
   standalone: true,
-  imports: [CommonModule, RouterModule,ReactiveFormsModule , FormsModule],
+  imports: [CommonModule, RouterModule, ReactiveFormsModule, FormsModule],
   templateUrl: './reset-otp.component.html',
-  styleUrl: './reset-otp.component.css'
+  styleUrl: './reset-otp.component.css',
 })
 export class ResetOtpComponent {
   email: any;
@@ -23,24 +29,25 @@ export class ResetOtpComponent {
     private toastr: ToastrService,
     private route: ActivatedRoute,
     private router: Router,
-    private fb:FormBuilder
+    private fb: FormBuilder
   ) {
     this.route.paramMap.subscribe((params) => {
       this.email = params.get('email');
-    if (!this.email || !this.isValidEmail(this.email)) {
-      this.toastr.error('Invalid email address.', 'Error');
-      this.router.navigate(['/signin/reset']);
-    }
+      if (!this.email || !this.isValidEmail(this.email)) {
+        this.toastr.error('Invalid email address.', 'Error');
+        this.router.navigate(['/signin/reset']);
+      }
     });
 
-    this.changePasswordForm = this.fb.group({
-      newPassword: ['', Validators.required,passwordValidator()],
-      confirmPassword: ['', Validators.required]
-    }, {
-      validator: this.passwordMatchValidator
-    });
-    
-
+    this.changePasswordForm = this.fb.group(
+      {
+        newPassword: ['', [Validators.required, passwordValidator()]],
+        confirmPassword: ['', Validators.required],
+      },
+      {
+        validator: this.passwordMatchValidator,
+      }
+    );
   }
   passwordMatchValidator(group: FormGroup) {
     const newPassword = group.get('newPassword')?.value;
@@ -48,48 +55,46 @@ export class ResetOtpComponent {
     return newPassword === confirmPassword ? null : { mismatch: true };
   }
 
-
   private isValidEmail(email: string): boolean {
     const emailPattern = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
     return emailPattern.test(email);
   }
 
-  OTP:string=""
+  OTP: string = '';
 
   onSubmit() {
-    let otpGet=document.getElementById('otp') as HTMLInputElement
+    let otpGet = document.getElementById('otp') as HTMLInputElement;
     let verify = {
       otp: otpGet.value.toString(),
-      email: this.email
-    }
+      email: this.email,
+    };
     this.otpService.verifyOtp(verify).subscribe({
       next: (response: any) => {
-        this.toastr.success(response)
+        this.toastr.success(response);
         this.ValidationCheck = true;
-      }, error: (error:any) => {
-        this.toastr.error(error.error || 'OTP verification failed. Please try again.')
-
-      }
-    })
-
-
+      },
+      error: (error: any) => {
+        this.toastr.error(
+          error.error || 'OTP verification failed. Please try again.'
+        );
+      },
+    });
   }
 
-
-  ChangePassword(){
-    let changePass={
-      email:this.email,
-      NewPassword:this.changePasswordForm.get('newPassword')?.value
-    }
-    console.log(changePass)
+  ChangePassword() {
+    let changePass = {
+      email: this.email,
+      NewPassword: this.changePasswordForm.get('newPassword')?.value,
+    };
+    console.log(changePass);
     this.otpService.changePassword(changePass).subscribe({
-      next:(response:any)=>{
-        this.toastr.success(response)
-        this.router.navigate(['/signin'])
-
-      },error:(error)=>{
-        this.toastr.error(error.error)
-      }
-    })
+      next: (response: any) => {
+        this.toastr.success(response);
+        this.router.navigate(['/signin']);
+      },
+      error: (error) => {
+        this.toastr.error(error.error);
+      },
+    });
   }
 }
